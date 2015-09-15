4d11e077c34e3335f4bf60c0ee1b63ab40e6982c
Guillermo Vargas <gavargas22@gmail.com>
2015-09-15 11:45:48 -0600

    Added buttons and other stuff

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,36 +1,31 @@
 <?php
 // No direct access
 defined('_JEXEC') or die;
+
+// $document = & JFactory::getDocument();
+//
+// $document->addStyleSheet("modules/mod_utepnews/assets/css/utep-news.css");
+// $document->addScript('modules/mod_utepnews/assets/jsx/news.jsx');
 ?>
 
 <div class="container-fluid no-padding center-custom-tabs-wrapper">
+
+  <div class="container-fluid center-tabs-background">
+    <div class="container">
+      <ul class="nav nav-tabs center-custom-nav-tabs" role="tablist">
+        <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">NEWS</a></li>
+        <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">EVENTS</a></li>
+        <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">MAJORS &amp; PROGRAMS</a></li>
+      </ul>
+    </div>
+  </div>
+
   <div class="container">
-
-    <ul class="nav nav-tabs center-custom-nav-tabs" role="tablist">
-      <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">NEWS</a></li>
-      <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">EVENTS</a></li>
-      <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">MAJORS &amp; PROGRAMS</a></li>
-    </ul>
-
     <div class="tab-content">
       <div role="tabpanel" class="tab-pane active" id="home">
-        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-          <ol class="carousel-indicators">
-            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
-            <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
-          </ol>
-
-          <div id="content"></div>
-
-          <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
+        <div id="content"></div>
+        <div class="bottom-btn">
+          <a href="#" class="btn btn-primary btn-more btn-sm" role="button">More News</a>
         </div>
       </div>
       <div role="tabpanel" class="tab-pane" id="profile">...</div>
