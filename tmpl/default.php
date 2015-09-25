49e0847714992d7dd2cbdb26791e8b515edc3e3e
Guillermo Vargas <gavargas22@gmail.com>
2015-09-25 10:33:33 -0600

    Stopped loading in component

--- tmpl/default.php
+++ tmpl/default.php
@@ -3,11 +3,11 @@
 defined('_JEXEC') or die;
 
 $doc = JFactory::getDocument();
-JHtml::_('jquery.framework');
-JHtml::_('stylesheet', JUri::root() . 'media/mod_utepnews/assets/css/utep-news.css');  // For CSS files
-JHtml::_('script', JUri::root() . 'media/mod_utepnews/assets/react/react-with-addons.js');       // For JS files
-JHtml::_('script', JUri::root() . 'media/mod_utepnews/assets/react/react.js');       // For JS files
-JHtml::_('script', JUri::root() . 'media/mod_utepnews/assets/react/JSXTransformer.js');       // For JS files
+// JHtml::_('jquery.framework');
+// JHtml::_('stylesheet', JUri::root() . 'media/mod_utepnews/assets/css/utep-news.css');  // For CSS files
+// JHtml::_('script', JUri::root() . 'media/mod_utepnews/assets/react/react-with-addons.js');       // For JS files
+// JHtml::_('script', JUri::root() . 'media/mod_utepnews/assets/react/react.js');       // For JS files
+// JHtml::_('script', JUri::root() . 'media/mod_utepnews/assets/react/JSXTransformer.js');       // For JS files
 // JHtml::_('script', JUri::root() . 'media/mod_utepnews/assets/jsx/news.js');
 ?>
 
