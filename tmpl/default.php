9b37157431a65bd8db9d82a5723b20fdb0afc1c0
Guillermo Vargas <gavargas22@gmail.com>
2016-01-20 12:18:05 -0700

    Changing button

--- tmpl/default.php
+++ tmpl/default.php
@@ -27,7 +27,7 @@
 			<ul class="nav nav-tabs center-custom-nav-tabs hidden-xs hidden-sm visible-md visible-lg" role="tablist">
 				<li role="presentation" class="active"><a href="#news" aria-controls="home" role="tab" data-toggle="tab" class="home-information-tabs">NEWS</a></li>
 				<li role="presentation"><a href="#events" aria-controls="profile" role="tab" data-toggle="tab" class="home-information-tabs">EVENTS</a></li>
-				<li role="presentation"><a href="#majorsprograms" aria-controls="messages" role="tab" data-toggle="tab" class="home-information-tabs">MAJORS &amp; PROGRAMS</a></li>
+				<li role="presentation"><a href="#majorsprograms" aria-controls="messages" role="tab" data-toggle="tab" class="home-information-tabs">ENGAGE</a></li>
 			</ul>
 			<?php //Responsive in cellphone code ?>
 			<div class="nav-center visible-xs visible-sm hidden-md hidden-lg">
