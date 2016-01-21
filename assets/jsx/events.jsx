43d80798b389de0f00dd9977e51d97272f89d569
Guillermo Vargas <gavargas22@gmail.com>
2016-01-21 12:18:41 -0700

    Events

--- assets/jsx/events.jsx
+++ assets/jsx/events.jsx
@@ -88,7 +88,7 @@ var EventSlides = React.createClass({
 
 var EventElement = React.createClass({
 	render: function() {
-		var classNames = "col-sm-6 item";
+		var classNames = "col-sm-4 item event-wrapper";
 		var articleImageStyle = {
 			backgroundImage: 'url(' + this.props.imagePath + ')',
 			backgroundSize: 'cover'
@@ -101,19 +101,17 @@ var EventElement = React.createClass({
 		return (
 
 			<div className={classNames} >
-				<a href={this.props.articleLink}>
 
-					<div className="col-lg-12 event-icon" style={articleImageStyle}>
-						<div className="picture-date-wrapper">
-							<div className="event-date-month">{months[new Date(Date.parse(this.props.articleStartMonth)).getMonth()]}</div>
-							<div className="event-date-day">{new Date(Date.parse(this.props.articleStartDay)).getDate()}</div>
-						</div>
+				<div className="col-lg-12 event-icon" style={articleImageStyle}>
+					<div className="picture-date-wrapper">
+						<div className="event-date-month">{months[new Date(Date.parse(this.props.articleStartMonth)).getMonth()]}</div>
+						<div className="event-date-day">{new Date(Date.parse(this.props.articleStartDay)).getDate()}</div>
 					</div>
-					<div className="col-lg-12 event-info-wrapper no-padding">
-						<div className="orange-strip" style={orangeStripCustomStyle}></div>
-						<div className="article-title-text">{this.props.articleTitle}</div>
-					</div>
-				</a>
+				</div>
+				<div className="orange-strip" style={orangeStripCustomStyle}></div>
+				<div className="event-title-text">{this.props.articleTitle}</div>
+				<div className="event-more-button"><a href={this.props.articleLink}>READ MORE &gt;</a></div>
+
 			</div>
 		)
 	}
