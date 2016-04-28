ff3ed0b7a3ef43ecebc6bcdd91473211110c6790
Guillermo Vargas <gavargas22@gmail.com>
2016-04-28 13:49:07 -0600

    Compensation

--- assets/jsx/events.js
+++ assets/jsx/events.js
@@ -85,8 +85,14 @@ var EventSlides = React.createClass({displayName: "EventSlides",
         evt.icon = imageServerURLPrefix.concat(evt.icon);
       }
       // Conversion to Mountain time
-      var convertedDate = new timezoneJS.Date(evt.start, 'America/Denver');
-
+      var timeFromJSON = new Date(evt.start);
+      // Get the offset caused by PHP
+      var offsetCausedByPHPModuleInHours = -(timeFromJSON.getTimezoneOffset())/60;
+      // Convert the UTC parsed time to milliseconds then substract the offset in milliseconds.
+      var compensatedEventDate = new Date(timeFromJSON.getTime() - (offsetCausedByPHPModuleInHours * 60 * 60 * 1000));
+      // Assign new date to the event object
+      console.log(compensatedEventDate);
+      // evt.start = compensatedEventDate;
       return (
         React.createElement(EventElement, {articleLink: eventURLPrefix.concat(evt.id), imagePath: "'" + evt.icon + "'", articleTitle: evt.name, articleExcerpt: evt.description, articleId: evt.id, articleStartDay: evt.start, articleStartMonth: evt.start, key: index})
       );
