f5050aeacf68cbec2e6081d86365f1dd1da76697
Guillermo Vargas <gavargas22@gmail.com>
2016-05-02 14:49:46 -0600

    removed

--- assets/javascripts/paginate.js
+++ assets/javascripts/paginate.js
@@ -47,40 +47,7 @@ var paginateNews = function(){
 var paginateEvents = function(){
   // Check to see if the element already has the slick class
   if (jQuery('.events-pagminated').hasClass('slick-initialized')) {
-    jQuery('.events-paginated').slick({
-      dots: true,
-      infinite: false,
-      speed: 300,
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      responsive: [{
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      }, {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          arrows: false
-        }
-      }, {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-      ]
-    });
+    // Do nothing
   } else {
     // Otherwise, apply slick.
     jQuery('.events-paginated').slick({
